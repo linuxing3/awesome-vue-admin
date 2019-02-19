ff85a7ed9cfc76294690748ec9212560a01bfe14
linuxing3 <linuxing3@qq.com>
2019-02-19 03:54:06 -0800

    router splitting

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,10 +14,14 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+      component: loadView('@/views', 'About')
     }
   ]
 })
+
+// route level code-splitting
+// this generates a separate chunk (about.[hash].js) for this route
+// which is lazy-loaded when the route is visited.
+function loadView (viewDir, ViewName) {
+  return require(/* webpackChunkName: "router" */ `${viewDir}/${ViewName}.vue`).default
+}
