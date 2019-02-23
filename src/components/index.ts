36d9d3fc6792ad33969a6d5bddd6fafd4a761977
linuxing3 <linuxing3@qq.com>
2019-02-23 12:12:13 -0400

    lazy

--- src/components/index.ts
+++ src/components/index.ts
@@ -26,13 +26,14 @@ const requireExamples = require.context(
   '@/components/examples', true, /\.vue$/
   )
   
-  requireExamples.keys().forEach(fileName => {
-    const componentExample = requireExamples(fileName)
-    const componentName = upperFirst(camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1')))
-    examples[componentName] = () => componentExample.default || componentExample
-    console.log('Lazy Registered ' + componentName)
+requireExamples.keys().forEach(fileName => {
+  const componentExample = requireExamples(fileName)
+  const componentName = 'Example' + upperFirst(camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1')))
+  examples[componentName] = () => componentExample.default || componentExample
 })
-
+  
 export const exampleList = keys(examples)
+console.log('Lazy Registered Components:')
+console.table(exampleList)
 
 export default examples
