86ee152902c667e5292134d16b776fc9e05da77d
linuxing3 <linuxing3@qq.com>
2019-03-19 07:46:22 -0400

    typed

--- src/util/statistic.ts
+++ src/util/statistic.ts
@@ -1,30 +1,64 @@
 import { uniq, mapKeys, keys } from 'lodash'
+
+interface IGenericField {
+  [ key: string ]: string | boolean | any
+}
+interface IStatisticDataSet {
+  [ key: string ]: number | string
+}
+
+interface IStatisticQuery {
+  fields: string[]
+  models: any
+  fieldsDefModel: string
+  fieldDef: string
+  queryModel: string
+  queryFieldName: string
+}
+
 /**
  * 使用一个模型的字段作为另一个模型特定字段的查阅向导，进行相关计数统计
+ * @param fields         [Array] 定义了查询字段数组
  * @param models         [object] 定义了模型集合
  * @param fieldsDefModel [string] 定义了查询字段的关系模型
+ * @param fieldsDef      [string] 定义了查询字段的关系模型
  * @param queryModel     [string] 将查询的模型
  * @param queryFieldName [string] 将查询的字段
  * @returns [object] 包含名称和统计量的对象, { type1: 10, type2: 10 }
  */
-export const modelStatistic = (
-  {
-    models,
-    fieldsDefModel,
-    fieldDef,
-    queryModel,
-    queryFieldName
-  }) => {
-  let fields: string[] = uniq(findValuesOfField(models[fieldsDefModel], fieldDef))
+export const modelStatistic = ({
+  fields,
+  models,
+  fieldsDefModel,
+  fieldDef,
+  queryModel,
+  queryFieldName
+}: IStatisticQuery) => {
+  let lookupFields: string[] = []
+  // get fields from lookup model if fields is undefined
+  if (fields === undefined) {
+    console.info('Fields is not defined, trying with lookup Model')
+    lookupFields = findValuesOfField(models[fieldsDefModel], fieldDef)
+  } else if (!Array.isArray(fields)) {
+    console.info('Fields is not Array, convert to array')
+    lookupFields = [ fields ]
+  } else {
+    lookupFields = fields
+  }
 
-  return fields.reduce(function (dataSet, fieldName) {
-    let fieldCount = models[queryModel]
-      .query()
-      .where(queryFieldName, fieldName)
-      .count()
-    dataSet[fieldName] = fieldCount
-    return dataSet
-  }, {})
+  try {
+    return lookupFields.reduce(function(dataSet, fieldName) {
+      let fieldCount = models[queryModel]
+        .query()
+        .where(queryFieldName, fieldName)
+        .count()
+      dataSet[fieldName] = fieldCount
+      return dataSet
+    }, {})
+  } catch (error) {
+    throw new Error(error)
+  }
+
 }
 
 /**
@@ -33,12 +67,13 @@ export const modelStatistic = (
  * @param fieldDef 字段名
  * @returns 某一字段的全部值组成的数组
  */
-export const findValuesOfField = (Model, fieldDef) => {
+export const findValuesOfField = (Model, fieldDef: string) => {
   let records = Model.query().get()
-  return records.reduce(function (fields, item) {
+  let fields: string[] = records.reduce(function(fields: any[], item: IGenericField) {
     fields.push(item[fieldDef])
     return fields
   }, [])
+  return uniq(fields)
 }
 
 /**
@@ -47,7 +82,7 @@ export const findValuesOfField = (Model, fieldDef) => {
  * @param fieldDef 字段名
  * @returns 某一字段的全部值组成的数组
  */
-export const _findValuesOfField = (Model, fieldDef) => {
-  let records = Model.query().get()
+export const _findValuesOfField = (Model, fieldDef: string) => {
+  let records: any[] = Model.query().get()
   return (uniq(keys(mapKeys(records, o => o[fieldDef]))))
 }
